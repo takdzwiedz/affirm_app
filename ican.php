0cccf2d2a031da17865624dee7a6f26166461495
takdzwiedz <a.kacprzak@tu-i-teraz.com.pl>
2018-03-02 17:07:09 +0100

    well done!

--- ican.php
+++ ican.php
@@ -1,16 +1,35 @@
 <!-- FORM DISPLAY -->
 <?php
 
-if(isset($_POST['send_ican']) && !empty($_POST['ican'])){
-    
+require_once 'config/Config.php';
+
+if (isset($_POST['send_ican']) && !empty($_POST['ican'])) {
+
     $ican = trim($_POST['ican']);
     $date = date("d-m-Y");
     $time = date("H:i:s");
-    
-    $ican = "INSERT INTO `affirmation`(`affirmation`, `date`, `time`) VALUES ('$ican', '$date', '$time')";
+
+    $icanQuery = "INSERT INTO `affirmation`(`affirmation`, `date`, `time`) VALUES ('$ican', '$date', '$time')";
     $insert = new DbConnect();
-    $save = $insert->db->query($ican);
-    
+    $save = $insert->db->query($icanQuery);
+
+    $connection = new DbConnect();
+    $request = "SELECT `mail` FROM `user` WHERE `is_active` = 1";
+    $result = $connection->db->query($request);
+    $lp = 0;
+
+    $sendICan = new SendMail(E_MAIL_ADMIN);
+
+    while ($row = $result->fetch_object()) {
+        $to=$row->mail;
+        $message = "Cześć!";
+        $subject = $ican;
+        $sendICan->send($to, $subject, $message);
+    }
+
+
+//    $sendMail->send($to, , $message);
+
     echo "<span> Brawo! <br> \"Mogę\" zostało wysłane!</span>";
 }
 ?>
