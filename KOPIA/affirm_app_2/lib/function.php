7345c372cf248c24be074a70d0b1ecc1bfca78f3
takdzwiedz.s <a.kacprzak@tu-i-teraz.com.pl>
2018-10-01 16:06:43 +0200

    Delete KOPIA from repo

--- KOPIA/affirm_app_2/lib/function.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function debug($var){
-    echo '<pre>';
-    var_dump($var);
-    echo '</pre>';
-}
--- KOPIA/affirm_app_2/page/thx.php
+++
@@ -1,7 +0,0 @@
-<h4>Super! Gwarantuję Ci wspaniałą przygodę. Zobaczysz!</h4>
-<p>
-    Teraz tylko potwierdź swoją rejestrację klikając na link z mejla, którego Ci wysłałem i zaczynamy!<br> 
-    Podekscytowany? Ja cholernie!<br>
-    Dobrego,<br>
-    Artur
-</p>
