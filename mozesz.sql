96fce1a64c4a0e65a4842d9a7db8a775c87229d4
Artur <a.kacprzak@tu-i-teraz.com.pl>
2019-09-07 21:54:10 +0200

    [MOZESZ][#41] Wgranie payu do vendora

--- mozesz.sql
+++ mozesz.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Czas generowania: 09 Sie 2019, 11:08
--- Wersja serwera: 5.7.26
--- Wersja PHP: 7.1.29
+-- Host: 193.218.152.241
+-- Czas generowania: 07 Wrz 2019, 21:46
+-- Wersja serwera: 5.7.27-30-log
+-- Wersja PHP: 7.0.33-0+deb9u3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `mozesz`
+-- Baza danych: `db100041556`
 --
+CREATE DATABASE IF NOT EXISTS `db100041556` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `db100041556`;
 
 -- --------------------------------------------------------
 
@@ -28,17 +30,14 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `affirmation_female`
 --
 
-DROP TABLE IF EXISTS `affirmation_female`;
-CREATE TABLE IF NOT EXISTS `affirmation_female` (
-  `id_affirmation` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `affirmation_female` (
+  `id_affirmation` int(11) NOT NULL,
   `affirmation` text COLLATE utf8_polish_ci NOT NULL,
   `author` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
   `user_rate` int(11) DEFAULT NULL,
   `date` varchar(15) COLLATE utf8_polish_ci NOT NULL,
   `time` varchar(10) COLLATE utf8_polish_ci NOT NULL,
-  `affirmation_male_id` int(11) NOT NULL,
-  PRIMARY KEY (`id_affirmation`),
-  KEY `affirmation_male_id` (`affirmation_male_id`)
+  `affirmation_male_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
@@ -47,15 +46,13 @@ CREATE TABLE IF NOT EXISTS `affirmation_female` (
 -- Struktura tabeli dla tabeli `affirmation_male`
 --
 
-DROP TABLE IF EXISTS `affirmation_male`;
-CREATE TABLE IF NOT EXISTS `affirmation_male` (
-  `id_affirmation` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `affirmation_male` (
+  `id_affirmation` int(11) NOT NULL,
   `affirmation` text COLLATE utf8_polish_ci NOT NULL,
-  `author` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
-  `user_rate` int(11) DEFAULT NULL,
+  `author` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `user_rate` int(11) NOT NULL,
   `date` varchar(15) COLLATE utf8_polish_ci NOT NULL,
-  `time` varchar(10) COLLATE utf8_polish_ci NOT NULL,
-  PRIMARY KEY (`id_affirmation`)
+  `time` varchar(10) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
@@ -64,14 +61,12 @@ CREATE TABLE IF NOT EXISTS `affirmation_male` (
 -- Struktura tabeli dla tabeli `history_user_affirmation`
 --
 
-DROP TABLE IF EXISTS `history_user_affirmation`;
-CREATE TABLE IF NOT EXISTS `history_user_affirmation` (
-  `hua_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `affirmation_id` int(11) NOT NULL,
+CREATE TABLE `history_user_affirmation` (
+  `hua_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `affirmation_id` int(11) DEFAULT NULL,
   `date` varchar(11) COLLATE utf8_polish_ci DEFAULT NULL,
-  `time` varchar(11) COLLATE utf8_polish_ci DEFAULT NULL,
-  PRIMARY KEY (`hua_id`)
+  `time` varchar(11) COLLATE utf8_polish_ci DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
@@ -80,10 +75,9 @@ CREATE TABLE IF NOT EXISTS `history_user_affirmation` (
 -- Struktura tabeli dla tabeli `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int(10) NOT NULL AUTO_INCREMENT,
-  `sex` text COLLATE utf8_polish_ci,
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `sex` varchar(1) COLLATE utf8_polish_ci DEFAULT NULL,
   `name` text COLLATE utf8_polish_ci NOT NULL,
   `city` varchar(30) COLLATE utf8_polish_ci NOT NULL,
   `mail` text COLLATE utf8_polish_ci,
@@ -93,9 +87,65 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_active` int(1) DEFAULT NULL,
   `security` varchar(60) COLLATE utf8_polish_ci DEFAULT NULL,
   `is_admin` int(11) DEFAULT NULL,
-  `genitive` varchar(45) COLLATE utf8_polish_ci DEFAULT NULL,
-  PRIMARY KEY (`id_user`)
+  `genitive` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `affirmation_female`
+--
+ALTER TABLE `affirmation_female`
+  ADD PRIMARY KEY (`id_affirmation`),
+  ADD KEY `affirmation_male_id` (`affirmation_male_id`);
+
+--
+-- Indeksy dla tabeli `affirmation_male`
+--
+ALTER TABLE `affirmation_male`
+  ADD PRIMARY KEY (`id_affirmation`);
+
+--
+-- Indeksy dla tabeli `history_user_affirmation`
+--
+ALTER TABLE `history_user_affirmation`
+  ADD PRIMARY KEY (`hua_id`);
+
+--
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `affirmation_female`
+--
+ALTER TABLE `affirmation_female`
+  MODIFY `id_affirmation` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `affirmation_male`
+--
+ALTER TABLE `affirmation_male`
+  MODIFY `id_affirmation` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `history_user_affirmation`
+--
+ALTER TABLE `history_user_affirmation`
+  MODIFY `hua_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ograniczenia dla zrzutów tabel
